0020240c43d4db6a3f4dd1d2b26cf700a6beffb1
Cory Brown <cory_s_brown@msn.com>
2015-12-05 19:50:17 -0800

    need to refine and add ingredients page

--- app/js/controllers/menu-item-info-controller.js
+++ app/js/controllers/menu-item-info-controller.js
@@ -4,6 +4,21 @@
  * # MenuItemInfoCtrl
  */
 angular.module('bellhappApp')
-    .controller('MenuItemInfoCtrl', function ($scope) {
 
-    });
+.controller('MenuItemInfoCtrl', function ($scope, $mdDialog) {
+    
+    $scope.todos = [
+      {
+        name: 'Cheese Pizza',
+        description: 'This is voted one of the best pizzas in town and is made with natural ingredients. To find' + 
+        ' more information about this food item, click the ingredients button below',
+      }
+    ];
+
+    $scope.toppings = [
+    { name: 'Pepperoni', wanted: false },
+    { name: 'Sausage', wanted: false },
+    { name: 'Black Olives', wanted: false },
+    { name: 'Green Peppers', wanted: false }
+  	];
+});
