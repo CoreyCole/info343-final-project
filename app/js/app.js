260feca27dce6754e41c2c840da5e35be8425d29
Corey Cole <coreyleoc@gmail.com>
2015-12-08 00:48:30 -0800

    update tablet interface - #10

--- app/js/app.js
+++ app/js/app.js
@@ -10,8 +10,14 @@ angular.module('bellhappApp', [
     ])
     .constant('firebaseUrl', 'https://info343-final.firebaseio.com/')
     .constant('testRestaurantID', '-K4bMCBfJkqlgK1LTNPz')
+    .constant('tableID', '-K4bEvRhNhh3LCWhRLGW')
     .constant('testTableID', '-K4bEvRhNhh3LCWhRLGW')
     .constant('brand', 'bellhapp')
+    .factory('currentTableRef', function(firebaseUrl, tableID) {
+        var rootRef = new Firebase(firebaseUrl);
+        var tablesRef = rootRef.child("tables");
+        return tablesRef.child(tableID);
+    })
     .factory('rootRef', function(firebaseUrl) {
         return new Firebase(firebaseUrl);
     })
