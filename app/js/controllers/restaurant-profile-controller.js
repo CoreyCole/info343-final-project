eecff5c190ff2ee9b3f7dfb638429624731c2e4b
arnavd96 <arnavdubey96@gmail.com>
2015-12-10 03:40:08 -0800

    picture and rating added

--- app/js/controllers/restaurant-profile-controller.js
+++ app/js/controllers/restaurant-profile-controller.js
@@ -5,8 +5,40 @@
  */
 angular.module('bellhappApp')
     .controller('RestaurantProfileCtrl', function(firebaseUrl, $firebaseArray, $scope, rootRef, testRestaurantRef, restKey, $state) {
-        $scope.restaurant = angular.fromJson(localStorage.getItem(restKey)) || [];
-        console.log($scope.restaurant);
+
+        $scope.renderAll = function(){
+            $scope.restaurant = angular.fromJson(localStorage.getItem(restKey)) || [];
+            console.log($scope.restaurant);
+
+            $scope.pictureUrl = $scope.restaurant.pictureUrl;
+            $scope.name = $scope.restaurant.name;
+            console.log($scope.name);
+
+            $scope.ratings = $scope.restaurant.ratings;
+            console.log($scope.ratings);
+
+            $scope.ratings = Object
+                .keys($scope.ratings)
+                .map(function (key) {
+                return $scope.ratings[key]
+            });
+            console.log($scope.ratings);
+
+            $scope.avgRating = 0;
+            var count = 0;
+            var total = 0;
+            $scope.ratings.forEach(function(rating){
+                console.log(rating);
+                count ++;
+                total += rating.score;
+                console.log(total);
+                $scope.avgRating = total / count;
+            });
+            console.log($scope.avgRating);
+
+        };
+
+        $scope.renderAll();
 
         $scope.refreshAll = function(){
             $scope.chosenRest = {};
